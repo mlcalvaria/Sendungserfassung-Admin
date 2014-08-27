d1717a7708f724418d06ef7c30ca6d7b6da01fdc
mlcalvaria <michael-luxenburger@t-online.de>
2014-08-27 14:16:16 +0200

    bugfix

--- production/login/loginCtrl.controller.js
+++ production/login/loginCtrl.controller.js
@@ -1,6 +1,6 @@
 loginModule.controller('loginCtrl', function($scope, user){
    $scope.login = function (){
-    user.login($scope.username, $scope.password)
-
+    user.login($scope.username, $scope.password);
+    console.log('123');
    };
 });
